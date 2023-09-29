72b2db28bdb31678070751a9c2a18dbb9d992c4e
Karthick M <mkarthick334.mk@gmail.com>
2023-09-29 08:02:47 +0000

    Math Js Testing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
+import { useEffect } from 'react';
 import './App.css';
 
+import * as mathjs from "mathjs";
+
 function App() {
+
+  useEffect(() => {
+    // functions and constants
+    console.log(mathjs.round(mathjs.e, 3));            // 2.718
+    console.log(mathjs.atan2(3, -3)); // mathjs.pi          // 0.75
+    console.log(mathjs.log(10000, 10));                  // 4
+    console.log(mathjs.sqrt(-4));                        // 2i
+  
+    // [[7, 0], [0, 7]]
+
+  
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    
     </div>
   );
 }
